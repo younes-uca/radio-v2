6260ec82a4e805a360b06af83e05801b9c64d0cc
younes.zouani <y.zouani@uca.ma>
2023-02-22 15:03:52 +0100

    fix bug : id ProtocoleInclusion et ProtocoleInclusionStatut doit garder le meme que le DMC

--- src/main/java/ma/enova/radio/zynerator/util/DateUtil.java
+++ src/main/java/ma/enova/radio/zynerator/util/DateUtil.java
@@ -169,7 +169,8 @@ public class DateUtil {
             if (date != null) {
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT_WITH_HOUR, Locale.ENGLISH);
                 OffsetDateTime off = OffsetDateTime.of(date, ZoneOffset.UTC);
-                ZonedDateTime zoned = off.atZoneSameInstant(ZoneId.of("UTC+1"));
+              //  ZonedDateTime zoned = off.atZoneSameInstant(ZoneId.of("UTC+1"));
+                ZonedDateTime zoned = off.atZoneSameInstant(ZoneId.of("UTC"));
                 return zoned.toLocalDateTime().format(formatter);
             }
         } catch (Exception e) {
